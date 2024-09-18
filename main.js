a9b723aebee08d93de85180f355cb3aab9811bf1
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-09-18 21:37:32 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-button').addEventListener('click', function(event){
+    event.preventDefault()
+
+    const inputItems = document.getElementById('input-items').value
+    console.log(inputItems)
+})
