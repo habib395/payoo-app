303eb52fd8923d9f18c939e54730046f4fec15df
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-09-18 22:11:34 +0600

    add home page

--- main.js
+++ main.js
@@ -2,5 +2,13 @@ document.getElementById('btn-button').addEventListener('click', function(event){
     event.preventDefault()
 
     const inputItems = document.getElementById('input-items').value
-    console.log(inputItems)
+    const inputPass = document.getElementById('input-pass').value
+    console.log(inputItems, inputPass)
+
+    if( inputItems === '5' && inputPass === '1234'){
+        console.log('You are Logged in')
+        window.location.href = 'home.html'
+    }else{
+        alert('Please enter right phone Number or PIN')
+    }
 })
